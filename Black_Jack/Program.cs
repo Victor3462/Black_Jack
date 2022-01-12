d452957cdc3a69d497c569a1d4978d2e8f8ce680
Victor Leiderö-Palmlöv <victorleideropalmlov@gmail.com>
2022-01-12 11:06:40 +0100

    Gjorde lite nödvändiga ints

--- Black_Jack/Program.cs
+++ Black_Jack/Program.cs
@@ -7,7 +7,13 @@ Raylib.SetTargetFPS(60);
 
 Color pokergreen = new Color(53,101,77,255);
 
+int playerMoney = 1000;
+int playerCardValue = 0;
+int dealerCardValue = 0;
 
+
+
+string currentMenu = "start";
 
 
 while(!Raylib.WindowShouldClose())
@@ -15,7 +21,13 @@ while(!Raylib.WindowShouldClose())
 
     Raylib.BeginDrawing();
 
-    Raylib.ClearBackground(pokergreen);
+    if(currentMenu == "start"){
+
+        Raylib.ClearBackground(pokergreen);
+        
+        Raylib.DrawText("BLACKJACK",200,125,75,Color.GOLD);
+
+    }
 
 
 
