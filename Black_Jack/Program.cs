31a508ef8f49083594ef08d39344ab4fedf99ae5
Victor Leiderö-Palmlöv <victorleideropalmlov@gmail.com>
2022-01-12 10:31:46 +0100

    Skapat Raylib grunder för att börja

--- Black_Jack/Program.cs
+++ Black_Jack/Program.cs
@@ -2,3 +2,28 @@
 using Raylib_cs;
 using System.Collections.Generic;
 
+Raylib.InitWindow(800,600,"Black Jack");
+Raylib.SetTargetFPS(60);
+
+Color pokergreen = new Color(53,101,77,255);
+
+
+
+
+while(!Raylib.WindowShouldClose())
+{
+
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(pokergreen);
+
+
+
+
+
+    Raylib.EndDrawing();
+
+
+
+
+}
